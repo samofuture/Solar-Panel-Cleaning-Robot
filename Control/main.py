e948369bfd36e725f28492ca142b1f91c32b3f67
samofuture <54423694+samofuture@users.noreply.github.com>
2023-09-25 10:04:27 -0400

    Update for library usage

--- Control/main.py
+++ Control/main.py
@@ -1,1 +1,10 @@
-from roboclaw_python import roboclaw_3
+from roboclaw_python.roboclaw_3 import Roboclaw as rc
+
+# Useful resource for roboclaw library: https://resources.basicmicro.com/using-the-roboclaw-python-library/
+
+if __name__ == '__main__':
+    left_motors = rc("/dev/ttyS0", 38400)
+    
+    left_motors.Open()
+    left_motors.ForwardMixed(0x80, 63)
+    left_motors.BackwardM1(0x80, )
