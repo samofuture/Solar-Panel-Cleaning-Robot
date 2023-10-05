d062432910802719bab3f527d9f3d0577645650d
Sam Crane <samovarfuture@gmail.com>
2023-10-04 21:30:24 -0400

    Update to expand remote control functionality with motors

--- Motor Control/roboclaw_python/roboclaw_mixedpwm.py
+++ Motor Control/roboclaw_python/roboclaw_mixedpwm.py
@@ -1,10 +1,10 @@
 import time
-from roboclaw import Roboclaw
+from roboclaw_3 import Roboclaw
 
 #Windows comport name
-rc = Roboclaw("COM9",115200)
+# rc = Roboclaw("COM9",115200)
 #Linux comport name
-#rc = Roboclaw("/dev/ttyACM0",115200)
+rc = Roboclaw("/dev/ttyACM0",38400)
 
 rc.Open()
 address = 0x80
