90c3940bdade959d38e10038c8a98ad03f45fd26
samofuture <54423694+samofuture@users.noreply.github.com>
2023-11-20 18:47:55 -0500

    Rework to stop using roboclaw library functions

--- motor_control.py
+++ motor_control.py
@@ -1,9 +1,10 @@
 from roboclaw_python.roboclaw_3 import Roboclaw as rc
+from gpiozero import Button
 import numpy as np
 
 # Useful resource for roboclaw library: https://resources.basicmicro.com/using-the-roboclaw-python-library/
 
-# Found out that there is a big delay that happens when roboclaws can't be found at that address
+# Found out that there is a big delay that happens when roboclaws can't be found at that address/serial port
 class MotorControl:
     """
     The MotorControl class is designed to run as a wrapper for the roboclaw library and for more straight 
@@ -12,17 +13,22 @@ class MotorControl:
     ppr: float = 1425.2                                     # Pulses per Revolution
     wheel_radius_cm = 2 * 2.54                              # The wheel radius in cm
     wheel_circumference_cm = 2 * wheel_radius_cm * np.pi    # The wheel circumference in cm
-    def __init__(self) -> None:
+    brush_speed = 64
+    auto_speed = 48
+    def __init__(self, manual_button: Button) -> None:
         self.right_motors = rc("/dev/ttyACM0", 38400)   # This is the motor controller with the main housing
         self.left_motors = rc("/dev/ttyACM1", 38400)
         self.brush_motor = rc("/dev/ttyACM2", 38400)
         self.address = 0x80
+        self.manual_button = manual_button
+
         right_status = self.right_motors.Open()
         left_status = self.left_motors.Open()
-        #brush_status = self.brush_motor.Open()
+        brush_status = self.brush_motor.Open()
+
         print("Right Motors Status:", right_status)
         print("Left Motors Status:", left_status)
-        #print("Brush Motors Status:", brush_status)
+        print("Brush Motors Status:", brush_status)
 
     def _distance_to_count(self, distance_cm: float) -> float:
         """
@@ -40,53 +46,82 @@ class MotorControl:
         distance_cm = rotations * self.wheel_circumference_cm
         return distance_cm
 
-    def move_robot_distance(self, speed: int, distance_cm: float):
+    def move_robot_distance(self, distance_cm: float, brush_enabled: bool = False):
         """
         Moves the robot at a given speed (0 - 126) for some distance given in cm
         Distance is negative to go one way, and positive to go the other way
+        Each time the function is called, the distance is relative to the robot
         """
-        final_enc_pos = int(self._distance_to_count(distance_cm))
+        self.stop_robot()
+        self.reset_encoders()
+        remaining_distance = abs(distance_cm)
+        if brush_enabled:
+            self.set_brush_speed('R', self.brush_speed)
         
-        self.right_motors.SpeedDistanceM1M2(self.address, speed, final_enc_pos, speed, final_enc_pos, 1)
-        self.left_motors.SpeedDistanceM1M2(self.address, speed, final_enc_pos, speed, final_enc_pos, 1)
+        while remaining_distance > 0 and not self.manual_button.is_pressed:
+            if distance_cm < 0:
+                self.move_robot('L', self.auto_speed)
+            else:
+                self.move_robot('R', self.auto_speed)
+
+            curr_distance = self.find_distance()
+            remaining_distance = abs(distance_cm) - curr_distance
+        
+        self.stop_robot()
+
+        if self.manual_button.is_pressed:
+            print(f"Stopped Robot at {curr_distance} with a remaining {remaining_distance} to travel")
+            print("Manual Mode Engaged")
+        else:
+            print(f"Stopped Robot at {curr_distance}")
 
     def stop_robot(self):
         """ 
-        Stops the Robot by setting their duty cycles to 0 (Should Coast)
+        Stops the Robot by setting their duty cycles to 0 (Should Coast (no active braking force))
         """
         self.right_motors.DutyM1M2(self.address, 0, 0)
         self.left_motors.DutyM1M2(self.address, 0, 0)
         self.brush_motor.DutyM1M2(self.address, 0, 0)
 
-    def move_brush(self, dir: str, speed: int) -> None:
+    def set_brush_speed(self, dir: str, speed: int) -> None:
         """
         Moves the brush only in a given direction ('R' or 'L') at a given speed (0 - 126)
         """
-        if dir == 'L':
+        # supposed to be 0-127 here but for some reason turning it backward runs into a
+        # Input/output error with the roboclaw library when at the full 127
+        if dir == 'R':
             self.brush_motor.ForwardM1(self.brush_motor, speed)
-        elif dir == 'R':
+        elif dir == 'L':
             self.brush_motor.BackwardM1(self.brush_motor, speed)
 
-    def move_right(self, speed: int):
-        print("Pre Move Right")
-        self.right_motors.ForwardM1(self.address, speed)
-        print("Post Move Right")
+    def set_right_speed(self, dir: str, speed: int):
+        """
+        Moves the right side of the robot in a direction at a speed
+        """
+        if dir == 'R':
+            self.right_motors.ForwardM1(self.address, speed)
+            self.right_motors.ForwardM2(self.address, speed)
+        else:
+            self.right_motors.BackwardM1(self.address, speed)
+            self.right_motors.BackwardM2(self.address, speed)
 
-    def move_left(self, speed: int):
-        print("Pre Move Left")
-        self.left_motors.ForwardMixed(self.address, speed)
-        print("Post Move Left")
+    def set_left_speed(self, dir: str, speed: int):
+        """
+        Moves the left side of the robot in a direction at a speed
+        """
+        if dir == 'R':
+            self.left_motors.ForwardM1(self.address, speed)
+            self.left_motors.ForwardM2(self.address, speed)
+        else:
+            self.left_motors.BackwardM1(self.address, speed)
+            self.left_motors.BackwardM2(self.address, speed)
 
     def move_robot(self, dir: str, speed: int):
         """
-        Meant for manual control, this moves the robot at a given speed (0 - 126) in a given direction ('R' or 'L')
+        Moves the robot at a given speed (0 - 126) in a given direction ('R' or 'L')
         """
-        if dir == 'R':
-            self.right_motors.ForwardMixed(self.address, speed)
-            self.left_motors.ForwardMixed(self.address, speed)
-        elif dir == 'L':
-            self.right_motors.BackwardMixed(self.address, speed)
-            self.left_motors.BackwardMixed(self.address, speed)
+        self.set_right_speed(dir, speed)
+        self.set_left_speed(dir, speed)
     
     def read_encoders(self) -> tuple:
         """
@@ -113,7 +148,7 @@ class MotorControl:
         left_dist = self._count_to_distance(np.mean(left_enc))
         right_dist = self._count_to_distance(np.mean(right_enc))
         if side == 'B':
-            return np.mean([left_dist, right_dist])
+            return np.mean([abs(left_dist), abs(right_dist)])
         elif side == 'L':
             return left_dist
         elif side == 'R':
@@ -125,5 +160,4 @@ class MotorControl:
         """
         The function to begin the autonomous cleaning routine
         """
-        self.move_brush('R', 32)
-        self.move_robot_distance('R', 20, 10)
+        self.move_robot_distance(15, True)
